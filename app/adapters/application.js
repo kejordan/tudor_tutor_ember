ff5a564649f32491120d7b94ff2716a3bd40dc9e
cmc33 <cmcguigan33@gmail.com>
2016-02-18 16:28:54 -0500

    simple auth done

--- app/adapters/application.js
+++ app/adapters/application.js
@@ -1,6 +1,9 @@
 import ActiveModelAdapter from 'active-model-adapter';
 
-export default ActiveModelAdapter.extend({
+import DataAdapterMixin from 'ember-simple-auth/mixins/data-adapter-mixin';
+
+export default ActiveModelAdapter.extend(DataAdapterMixin, {
   namespace: "api/v1",
-  
+  authorizer: 'authorizer:devise'
+
 });
