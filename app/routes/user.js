888eb02d58824912561bba8f6fad677f1581e959
kjordan88 <kristy.jordan88@gmail.com>
2016-02-20 18:03:34 -0500

    attempts to add bpd show page and login

--- app/routes/user.js
+++ app/routes/user.js
@@ -3,5 +3,12 @@ import Ember from 'ember';
 export default Ember.Route.extend({
     model(){
       return "Your name";
-    }
+    },
+    actions:{
+  getCurrentUser(){
+    this.store.findRecord('user', 'me').then((user => {
+      this.transitionTo('users/user_id');
+    }));
+  }
+}
 });
