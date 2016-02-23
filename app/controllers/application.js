f7df593c1a2061d71ff1194bd20a5828ae5d7037
psswf1172 <jennifermabella@gmail.com>
2016-02-23 15:40:51 -0500

    post and postContent added

--- app/controllers/application.js
+++ app/controllers/application.js
@@ -4,12 +4,12 @@ export default Ember.Controller.extend({
   currentUser:null,
   session: Ember.inject.service('session'),
 
-  updateCurrentUser: function () {
-    if (this.get("session.isAuthenticated")) {
-      this.get('store').findRecord('user', 'me').then((user) => {
-        user = this.store.createRecord('user', {email: user._internalModel._data.email, password: user._internalModel._data.password});
-        this.set('currentUser', user);
-      });
-    }
-  }.on('init')
+  // updateCurrentUser: function () {
+  //   if (this.get("session.isAuthenticated")) {
+  //     this.get('store').findRecord('user', 'me').then((user) => {
+  //       user = this.store.createRecord('user', {email: user._internalModel._data.email, password: user._internalModel._data.password});
+  //       this.set('currentUser', user);
+  //     });
+  //   }
+  // }.on('init')
 });
