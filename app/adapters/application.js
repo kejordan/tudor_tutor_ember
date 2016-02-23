e5cc903cbd2145ed59bb293e7706842cad8eef84
psswf1172 <jennifermabella@gmail.com>
2016-02-22 20:12:52 -0500

    old changes

--- app/adapters/application.js
+++ app/adapters/application.js
@@ -1,15 +1,18 @@
-// import DS from 'ember-data';
+import DS from 'ember-data';
 
 
 // import DataAdapterMixin from 'ember-simple-auth/mixins/data-adapter-mixin';
-// // export default DS.JSONAPIAdapter.extend(DataAdapterMixin, {
-// //   authorizer: 'authorizer:devise'
-// // });
+// export default DS.JSONAPIAdapter.extend(DataAdapterMixin, {
+//   namespace: "api/v1",
+//   host: "http://localhost:3000",
+//   authorizer: 'authorizer:devise'
+// });
 
 
 import ActiveModelAdapter from 'active-model-adapter';
 export default ActiveModelAdapter.extend({
   namespace: "api/v1",
+  host: "http://localhost:3000",
   authorizer: 'authorizer:devise'
 
 });
