605249379f68c7c5a393e82ddf5cf13467cf5d75
kjordan88 <kristy.jordan88@gmail.com>
2016-02-22 21:05:31 -0500

    adds more bootswatch elements

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -4,7 +4,7 @@ var EmberApp = require('ember-cli/lib/broccoli/ember-app');
 
 module.exports = function(defaults) {
   var app = new EmberApp(defaults, {
-    'ember-cli-bootswatch': {'theme': 'united'}
+    'ember-cli-bootswatch': {'theme': 'lumen'}
     // Add options here
   });
 
